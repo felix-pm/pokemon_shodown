f4b14266c2af3200862755102d690b56b725fe1f
fefe <felix.parendeau@gmail.com>
2026-03-12 15:40:04 +0100

    add connection database

---
+++ src/main/java/com/example/projet/controller.java
@@ -0,0 +1,41 @@
+package com.example.projet;
+
+import java.sql.*;
+
+public class controller {
+
+    public controller(){
+        final String DB_URL = "jcdb://localhost:8889/Coda_POKEMON_SHODOWN";
+        final String USER = "root";
+        final String PASS = "root";
+
+
+
+        try {
+            Connection connection = DriverManager.getConnection(DB_URL, USER, PASS);
+            Statement stmt = connection.createStatement();
+            String query = "SELECT * FROM Pokemon JOIN Pokemon_Attaque ON Pokemon.id = " +
+                    "Pokemon_Attaque.pokemon_id JOIN Attaque ON Attaque.id = Pokemon_Attaque.attaque_id";
+            ResultSet rs = stmt.executeQuery(query);
+            while(rs.next()){
+                System.out.println(rs.getString("nom_pokemon"));
+                System.out.println(rs.getInt("hp"));
+                System.out.println(rs.getInt("attaque"));
+                System.out.println(rs.getInt("attaque_speciale"));
+                System.out.println(rs.getInt("defense"));
+                System.out.println(rs.getInt("defense_speciale"));
+                System.out.println(rs.getInt("vitesse"));
+                System.out.println(rs.getString("type1"));
+                System.out.println(rs.getString("type2"));
+                System.out.println(rs.getString("src"));
+                System.out.println("----------------");
+                System.out.println(rs.getString("nom_attaque"));
+                System.out.println(rs.getInt("puissance"));
+                System.out.println(rs.getString("type"));
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.projet {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens com.example.projet to javafx.fxml;
